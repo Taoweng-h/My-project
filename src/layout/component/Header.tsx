5bdaed20d48e5132ea9f91668431ed8ae0c453d3
hkl <1597863376@qq.com>
2022-05-19 16:12:10 +0800

    头部菜单样式完成

--- src/layout/component/Header.tsx
+++ src/layout/component/Header.tsx
@@ -1,7 +1,30 @@
+import { Menu, Dropdown, Space } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 
 function Header(){
+
+  const menu = (
+    <Menu items={[
+      {
+        label:'退出',
+        key:'0'
+      }
+    ]} />
+  )
   return (
-    <div className="header">header</div>
+    <div className="header">
+      <div className="logo">
+        logo
+      </div>
+      <Dropdown overlay={menu} trigger={['click']} className="head-dropdown">
+        <a onClick={e => e.preventDefault()}>
+          <Space>
+            click me
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
+    </div>
   )
 }
 
